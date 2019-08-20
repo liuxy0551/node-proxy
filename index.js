32605e5adbd57cc2996c51eb80f2f364a979cc87
liuyi <liuxy0551@qq.com>
2019-08-20 17:30:40 +0800

    rename

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let http = require('http');
 let httpProxy = require('http-proxy');
 let proxy = httpProxy.createProxyServer({});
-let portList = require('./portList')
+let ports = require('./ports')
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
   // 设置 Header
   proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
 });
 
-for (let i of portList) {
+for (let i of ports) {
   http.createServer(function(req, res) {
     proxy.web(req, res, {
       target: i.target
     });
   }).listen(i.port);
+
   console.log(`listening ${i.name} on port ${i.port}`);
 }

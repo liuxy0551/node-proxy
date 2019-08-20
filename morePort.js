59b0dc8afb5136560085e26653370e87a4e8d7bb
liuyi <liuxy0551@qq.com>
2019-08-20 22:43:10 +0800

    .

--- morePort.js
+++ morePort.js
@@ -3,11 +3,13 @@ let proxy = require('http-proxy').createProxyServer({});
 let ports = require('./ports');
 
 for (let i of ports) {
-  http.createServer(function(req, res) {
-    proxy.web(req, res, {
-      target: i.target
-    });
-  }).listen(i.port);
+  if (!i.hidden) {
+    http.createServer(function(req, res) {
+      proxy.web(req, res, {
+        target: i.target
+      });
+    }).listen(i.port);
 
-  console.log(`listening ${i.name} on port ${i.port}`);
+    console.log(`listening ${i.name} on port ${i.port}`);
+  }
 }

22f87487473a71832f2d6bebbe799ec5a36d28a9
liuyi <liuxy0551@qq.com>
2019-08-20 21:06:54 +0800

    init

--- morePort.js
+++ morePort.js
@@ -1,12 +1,6 @@
 let http = require('http');
-let httpProxy = require('http-proxy');
-let proxy = httpProxy.createProxyServer({});
+let proxy = require('http-proxy').createProxyServer({});
 let ports = require('./ports');
-
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  // 设置 Header
-  proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
-});
 
 for (let i of ports) {
   http.createServer(function(req, res) {

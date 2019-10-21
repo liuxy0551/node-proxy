334809a281311cbf7bbc986441a9bebfd90b4c56
liuyi <liuxy0551@qq.com>
2019-10-21 08:41:18 +0800

    remove Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -2,18 +2,6 @@ let http = require('http');
 let proxy = require('http-proxy').createProxyServer({});
 let ports = require('./ports');
 
-// 解决跨域
-proxy.on('proxyRes', function (proxyRes, req, res) {
-  // res.setHeader('content-type', 'application/json;charset=utf-8');
-  Object.keys(proxyRes.headers).forEach(key => {
-    res.setHeader(key, proxyRes.headers[key]);
-  })
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Proxy-Target, Content-Type, Authorization, Auth, Token, Access-Token, Access_Token, AccessToken, Code');
-  proxyRes.pipe(res)
-});
-
 for (let i of ports) {
   if (!i.hidden) {
     http.createServer(function(req, res) {
